aa856282e04c201902534889b79d206179958f74
Roshan <roshansahu28062005@gmail.com>
2026-06-12 22:26:29 +0530

    initial stack push/pop with add and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello World!!");
+    // why vec<i64> for the stack?
+    // A stack only needs to add/remove from the top.
+    // both push() and pop() to do on a Vec are fast     (O(1)).
+    // No need for a special "stack" type — Vec already behaves like one
+    let mut stack: Vec<i64> = Vec::new();
+
+    stack.push(7);
+    stack.push(3);
+
+    // let b: Option<i64>= stack.pop();
+    // let a: Option<i64>= stack.pop(); -> cannot add a + b as (Option<i64> + Option<i64>) can't be added as Option<i64> returns a container either Some(T), or None
+
+    //That's why we need .unwrap() first — it opens the box and gives you the i64 inside (or crashes if the box was empty/None):
+
+    let b = stack.pop().unwrap();
+    let a = stack.pop().unwrap();
+
+    stack.push(a + b);
+
+    let result = stack.pop().unwrap();
+    println!("result: {}", result);
 }
